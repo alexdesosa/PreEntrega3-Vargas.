005509e9bd4706ef3b69520f08bebad0967fc110
alexdesosa <47509346+alexdesosa@users.noreply.github.com>
2024-02-06 19:04:38 -0300

    Add files via upload

---
+++ css/styles.cs
@@ -0,0 +1,19 @@
+* {
+  font-family: "Bree Serif", serif;
+}
+
+header nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: flex-end;
+}
+header nav ul li a {
+  text-decoration: none;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+}
+
+/*# sourceMappingURL=styles.cs.map */
